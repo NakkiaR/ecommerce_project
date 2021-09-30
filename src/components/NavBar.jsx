5c91aee6543997ee4d3165e8af861e105e4472c1
Djaypm89 <djaypm89@gmail.com>
2021-09-30 11:58:35 -0400

    NavBar file code created

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,32 @@
+const NavigationBar = ({user}) => {
+    return (
+        <div>
+            {user && <h4>Welcome {user.username}</h4>}
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/profile'>Profile</Link>
+                </li>
+                {!user &&
+                    <React.Fragment>
+                        <li>
+                            <Link to='/register'>Register</Link>
+                        </li>
+                        <li>
+                            <Link to='/login'>Login</Link>
+                        </li>
+                    </React.Fragment>
+                }
+                {user &&
+                    <React.Fragment>
+                        <li>
+                            <Link to='/logout'>Logout</Link>
+                        </li>
+                    </React.Fragment>
+                }
+            </ul>
+        </div>
+    );
+}
